b1aa46b5c7560cd0df7d793cb877aae60e2b1702
Fawwaz RN <fawwaz1511@student.abudzar.sch.id>
2026-02-23 10:02:50 +0700

    sortByDate

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -3,26 +3,25 @@ import { project as myProject } from "./asset";
 
 export default function MyProject() {
   const [searchTerm, setSearchTerm] = useState("");
-
-  const filteredProjects = myProject.filter(
-    (item) =>
-      item.nama.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      item.slogan.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  const filteredAndSortedProjects = myProject
+    .filter(
+      (item) =>
+        item.nama.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.slogan.toLowerCase().includes(searchTerm.toLowerCase()),
+    )
+    .sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal)); // Menggunakan b - a untuk descending
   return (
     <section
       className="top-0 left-0 relative max-md:sticky flex justify-end items-center bg-utama max-md:bg-transparent w-full h-screen font-jakarta"
       id="myProject">
       <div className="flex justify-center items-center max-md:p-0 px-12.5 py-6.25 w-4/5 max-md:w-full h-full">
         <div className="flex flex-col justify-between items-center gap-10 bg-white px-6.25 py-9 border border-black rounded-3xl w-full h-full">
-          {/* Bagian Header & Search Bar */}
           <div className="flex max-md:flex-col justify-between items-center max-md:gap-4 w-full">
             <span className="hidden md:block bg-black w-full h-px"></span>
             <div className="flex flex-col items-center gap-4 w-full md:w-1/2 font-bold text-center">
               <h1 className="text-2xl tracking-[10px]">BAB 3</h1>
               <h2 className="text-3xl">My Project</h2>
             </div>
-
             <span className="hidden md:block bg-black w-full h-px"></span>
           </div>
           {/* Input Pencarian */}
@@ -32,10 +31,9 @@ export default function MyProject() {
             className="px-4 py-2 border border-black rounded-full focus:outline-none focus:ring-2 focus:ring-black w-full font-normal text-sm"
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          {/* List Project */}
           <div className="flex justify-start items-center gap-10 max-md:gap-3 w-full h-full overflow-x-scroll text-white scrollbar-hide">
-            {filteredProjects.length > 0 ? (
-              filteredProjects.map((item) => (
+            {filteredAndSortedProjects.length > 0 ? (
+              filteredAndSortedProjects.map((item) => (
                 <a
                   key={item.nama}
                   href={item.link}
